d9156fd82c8665e4d6d0a1c4a40599401b34af13
Marcin Stanisławek <mstanislawek@gmail.com>
2019-08-13 00:09:54 +0200

    Updates

---
+++ precache-manifest.37a8ab894b5c5e5972fe21ae1d74e304.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb07f0aebed3687f25eb5a63e00efea4",
+    "url": "/bmi/index.html"
+  },
+  {
+    "revision": "0ffdb3aba3999fb51631",
+    "url": "/bmi/static/css/main.cf8dd2f4.chunk.css"
+  },
+  {
+    "revision": "dff8f5e6896d4e47fb65",
+    "url": "/bmi/static/js/2.d16ba94f.chunk.js"
+  },
+  {
+    "revision": "0ffdb3aba3999fb51631",
+    "url": "/bmi/static/js/main.7322d6b7.chunk.js"
+  },
+  {
+    "revision": "1261b34cd10412fc437e",
+    "url": "/bmi/static/js/runtime~main.7be4808b.js"
+  }
+]);
